4ce33456d97aaa27be2d5e2c9329d3fc6fa9b344
Johan Öbrink <johan.obrink@24hr.se>
2012-03-07 08:12:48 +0100

    Added vows for testing

---
+++ tests/gps-tracker-tests.js
@@ -0,0 +1,26 @@
+// gps-tracker-tests.js
+
+var vows = require('vows'),
+    assert = require('assert');
+
+var tracker = new require('./gpsTracker.js').Tracker();
+
+// Create a Test Suite
+vows.describe('GpsTracker').addBatch({
+    'when converting NMEA 0302.78469 to decimal': {
+        topic: function () { return '0302.78469' },
+
+        'we get 3.046412': function (topic) {
+            assert.equal (tracker.nmeaToDecimal(topic), 3.046412);
+        }
+    },
+    'when converting NMEA 10141.82531 to decimal': {
+        topic: function () { return 0 / 0 },
+
+        'we get 101.6971': {
+            'is not a number': function (topic) {
+                assert.equal (tracker.nmeaToDecimal(topic), 101.6971);
+            }
+        }
+    }
+}).run(); // Run it
