3f2afdd7d4c81a0b547bf83097b1e70b41b802e7
Johan Öbrink <johan.obrink@24hr.se>
2012-06-13 19:36:53 +0200

    socketConnector listens for connect

--- test/socketConnector-tests.js
+++ test/socketConnector-tests.js
@@ -1,15 +1,34 @@
-var should = require('should');
+var should = require('should'),
+	eventTrigger = require('../lib/eventTrigger');
 
-var connector;
+var connector, io, trigger;
 
 describe('SocketConnector', function() {
 
 	beforeEach(function() {
+		io = {
+			sockets: {}
+		};
+
+		trigger = eventTrigger.create(io.sockets);
 		connector = require('../lib/socketConnector').create();
-	});
-
-	it('listens for "connected" event', function() {
 
 	});
+
+	it('calls registerListeners after "connection" event', function(done) {
+
+		var s = {};
+
+		connector.registerListeners = function(socket) {
+			socket.should.equal(s);
+			done();
+		}
+
+		connector.connect(io);
+		trigger('connection', s);
+
+	});
+
+	//describe('#registerListeners')
 
 });
